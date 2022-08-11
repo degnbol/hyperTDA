0443af8984bf395a381711d0d2f7b386866a8833
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-11 13:57:46 +1000

    print comments before each step

--- examples/default_pipeline/RUNME.sh
+++ examples/default_pipeline/RUNME.sh
@@ -8,18 +8,18 @@ cd $0:h
 # start with a folder pointClouds/ with .npy or .tsv files where each file is a 
 # matrix with 3 columns.
 
-# calculate persistent homology
+echo "persistent homology"
 xyz2PH.jl pointClouds/ PH/
 
-# get hypergraphs
+echo "get hypergraphs"
 PH2hypergraph.jl PH/ H/
 
-# get node centralities from hypergraphs created from persistent homology
+echo "get node centralities from hypergraphs created from persistent homology"
 HG_nodeCent.jl PH/ nodeCents/
 
-# community detection
+echo "community detection"
 louvain.py PH/ pointClouds/ communities.json
 
-# then open jupyter notebook and view communities and centrality
+echo "then open jupyter notebook and view communities and centrality"
 jupyter notebook result_visualisation.ipynb
 
