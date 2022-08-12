bb56ea058a7b981d62ceb342b3e818ab7e8c6d50
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-12 16:42:18 +1000

    CNN instructions

---
+++ results/AnDi/CNN.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+ROOT=`git root`
+export PATH="$PATH:$ROOT/src"
+
+# unzip if not already done
+for file in Model_?/{H,nodeCents}.zip; do
+    if [ ! -d $file:r ]; then
+        unzip $file -d $file:h
+        rm -r $file:h/__MACOSX
+    fi
+done
+
+mkdir -p CNN
+
+hypergraph_CNN.jl --cv -m 8 -k 2 5 10 15 20 -f 64 -F 32 -H Model_?/H/ -V Model_?/nodeCents/ --pred=CNN/pred.tsv
+
