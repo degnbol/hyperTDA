bc8a5d590118d4dd53534421bc7373b0ce92b11a
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-09-09 18:59:27 +1000

    goto root if not

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+cd $0:h
 # make a command "git root" that gives the root folder of the repo.
 git config alias.root 'rev-parse --show-toplevel'
 # make sure submodules of other publications are initialized.
