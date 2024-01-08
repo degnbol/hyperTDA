8ce82cf4ebc0167f51f7227166cdf1b5a7e75af4
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-08 16:27:28 +1100

    CNN all combinations fixed

--- results/AnDi/matroid_generators/CNN/CNN_0H.sh
+++ results/AnDi/matroid_generators/CNN/CNN_0H.sh
@@ -6,5 +6,5 @@ date
 
 mkdir -p models/
 # has to be julia v1.8.3
-$HOME/packages/julias/julia-1.8/bin/julia $ROOT/src/hypergraph_CNN.jl --cv -m 8 -k 2 5 10 15 20 -f 64 -F 32 -e 500 -E 100 --0H -H ../Model_?/H/ -V ../Model_?/nodeCents/ --pred=pred_0V.tsv --save-model models/model_0V.bson
+$HOME/packages/julias/julia-1.8/bin/julia $ROOT/src/hypergraph_CNN.jl --cv -m 8 -k 2 5 10 15 20 -f 64 -F 32 -e 500 -E 100 --0H -H ../Model_?/H/ -V ../Model_?/nodeCents/ --pred=pred_0H.tsv --save-model models/model_0H.bson
 
--- results/AnDi/matroid_generators/CNN/CNN_0H_134.sh
+++ results/AnDi/matroid_generators/CNN/CNN_0H_134.sh
@@ -6,5 +6,5 @@ date
 
 mkdir -p models/
 # has to be julia v1.8.3
-$HOME/packages/julias/julia-1.8/bin/julia $ROOT/src/hypergraph_CNN.jl --cv -m 8 -k 2 5 10 15 20 -f 64 -F 32 -e 500 -E 100 --0H -H ../Model_{1,3,4}/H/ -V ../Model_{1,3,4}/nodeCents/ --pred=pred_0V_134.tsv --save-model models/model_0V_134.bson
+$HOME/packages/julias/julia-1.8/bin/julia $ROOT/src/hypergraph_CNN.jl --cv -m 8 -k 2 5 10 15 20 -f 64 -F 32 -e 500 -E 100 --0H -H ../Model_{1,3,4}/H/ -V ../Model_{1,3,4}/nodeCents/ --pred=pred_0H_134.tsv --save-model models/model_0H_134.bson
 
