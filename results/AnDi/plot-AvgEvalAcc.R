17c1af517289aca0ff9c99a32267d2a142ce21df
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-01-09 18:41:17 +1100

    avg eval acc detail figs

--- results/AnDi/plot-AvgEvalAcc.R
+++ results/AnDi/plot-AvgEvalAcc.R
@@ -26,22 +26,45 @@ ggsave("avgEvalAcc.pdf", width=5, height=5)
 # Only showing for input=H as V is clearly worse.
 # Only showing for matroid as that's what we did before.
 
+dtHV = fread("./matroid_generators/CNN/pred.tsv")
+dtHV134 = fread("./matroid_generators/CNN/pred_134.tsv")
 dt0V = fread("./matroid_generators/CNN/pred_0V.tsv")
 dt0V134 = fread("./matroid_generators/CNN/pred_0V_134.tsv")
+dtHV[, c("Models", "Input"):=.("all 5", "H+V")]
+dtHV134[, c("Models", "Input"):=.("1,3,4", "H+V")]
+dt0V[, c("Models", "Input"):=.("all 5", "H")]
+dt0V134[, c("Models", "Input"):=.("1,3,4", "H")]
+dt = rbind(dtHV, dtHV134, dt0V, dt0V134)
 
-dt0V = dt0V[, .N, by=c("Pred", "Label")]
-dt0V134 = dt0V134[, .N, by=c("Pred", "Label")]
+dt = dt[, .N, by=c("Pred", "Label", "Models", "Input")]
 
-modelNames = c("a", "b", "c", "d", "e")
+# 1,3,4 is zero indexed so we need 2,4,5. The file has 1,2,3.
+dt[Models=="1,3,4", c("Pred", "Label"):=.(Pred+1, Label+1)]
+dt[(Models=="1,3,4") & (Pred>2), Pred:=Pred+1]
+dt[(Models=="1,3,4") & (Label>2), Label:=Label+1]
 
-ggplot(dt0V134, aes(x=Pred, y=Label, fill=N, label=N)) +
-    geom_tile() +
-    geom_label(aes(color=N>2000), label.size=NA) +
-    scale_fill_gradient(low="white", high="#412877", guide=NULL) +
-    scale_color_manual(values=c(`FALSE`="black", `TRUE`="white"), guide=NULL) +
-    scale_x_continuous(name="Predicted model", breaks=c(1,2,3), labels=modelNames[c(1,3,4)], expand=c(0,0)) +
-    scale_y_continuous(name="True model", breaks=c(1,2,3), labels=modelNames[c(1,3,4)], expand=c(0,0), trans="reverse") +
-    theme_minimal() +
-    theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
+modelNames = c("ATTM", "CTRW", "FBM", "LW", "SBM")
+dt[, "Predicted model":=modelNames[Pred]]
+dt[, "True model":=modelNames[Label]]
 
+plt = function(df) {
+    ggplot(df, aes(x=`Predicted model`, y=`True model`, fill=N, label=N)) +
+        geom_tile() +
+        geom_label(aes(color=N>2000), label.size=NA) +
+        scale_fill_gradient(low="white", high="#58228e", guide=NULL) +
+        scale_color_manual(values=c(`FALSE`="black", `TRUE`="white"), guide=NULL) +
+        scale_x_discrete(expand=c(0,0)) +
+        scale_y_discrete(expand=c(0,0)) +
+        theme_minimal() +
+        theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
+}
+
+plt(dt[(Models=="all 5") & (Input=="H+V")])
+ggsave("avgEvalAcc_All5_HV.pdf", width=5, height=5)
+plt(dt[(Models=="all 5") & (Input=="H")])
+ggsave("avgEvalAcc_All5_H.pdf", width=5, height=5)
+plt(dt[(Models=="1,3,4") & (Input=="H+V")])
+ggsave("avgEvalAcc_134_HV.pdf", width=5, height=5)
+plt(dt[(Models=="1,3,4") & (Input=="H")])
+ggsave("avgEvalAcc_134_H.pdf", width=5, height=5)
 
