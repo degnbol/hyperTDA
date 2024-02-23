5ef0c1b94dd3326262268e838946fae821f9f1d7
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-02-23 17:30:37 +1100

    got minimal running

--- examples/for_agnese/minimal/PH_minimal.sh
+++ examples/for_agnese/minimal/PH_minimal.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 # on spartan to use gurobi
 # Gurobi.jl also can only be installed after loading module
-module load Gurobi
-
-PLPH=~/PLPH
-$PLPH/src/xyz2PH_minimal.jl example_3.npy PH_minimal/
+# module load Gurobi
+~/PLPH/src/xyz2PH_minimal.jl example_3.npy PH_minimal/
 
