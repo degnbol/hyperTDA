afa1d30ffffd415792bb30424516c2cec367f1ff
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-02-23 16:42:47 +1100

    minimal

---
+++ examples/for_agnese/RUNME.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+ROOT=`git root`
+export PATH="$PATH:$ROOT/src"
+cd $0:h
+
+# start with a folder pointClouds/ with .npy or .tsv files where each file is a 
+# matrix with 3 columns.
+
+echo "persistent homology"
+xyz2PH.jl pointClouds/ PH/
+
+echo "get hypergraphs"
+PH2hypergraph.jl PH/ H/
+
+echo "get node centralities from hypergraphs created from persistent homology"
+HG_nodeCent.jl PH/ nodeCents/
+
+echo "community detection"
+louvain.py PH/ pointClouds/ communities.json
+
+echo "then open jupyter notebook and view communities and centrality"
+jupyter notebook result_visualisation.ipynb
+
---
+++ examples/for_agnese/minimal/PH_minimal.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# on spartan to use gurobi
+module load Gurobi && xyz2PH_minimal.jl example_3.npy PH_minimal/
+
